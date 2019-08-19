5ad99631375c4e4d745c806acb7ae048420928b5
Inno97 <daniel-tan2009@hotmail.com>
2019-08-20 02:31:17 +0800

    Level 1-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,7 @@
  * but we'll split up everything later on
  */
 
+import java.io.*;
 import java.util.*;
 
 public class Duke {
@@ -26,32 +27,68 @@ public class Duke {
         //local variables to keep track of
         boolean exitFlag = false;
 
-        String command; //user command
-        String commandType; //lowercase command for duke to interpret
+        String input; //user input
+        String command; //if applicable, split into command and parameter
+        String inputParams;
 
-        String taskList[]; //store up to 100 tasks for now
-        taskList = new String[100];
+        ArrayList<String> taskList = new ArrayList<String>(0);
         int numTasks = 0;
 
-        //duke level 1-2: add, list
-        //Add the ability to store whatever text entered by the user and display them back to the user when requested.
+        //duke level 1-3: mark as done
+        //Add the ability to mark tasks as done.
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         while (!exitFlag) {
-            command = sc.nextLine();
+            input = sc.nextLine();
 
-            //parse command
-            commandType = command.toLowerCase();
+            //parse input
+            String inputParsed[] = input.split(" ", 2);
+            command = inputParsed[0];
+            if (inputParsed.length == 1) {
+                inputParams = ""; //keep empty for now
+            } else {
+                inputParams = inputParsed[1];
+            }
+            command = command.toLowerCase();
 
-            switch(commandType) {
+            switch(command) {
                 case "list": //list all tasks in duke
                         System.out.println("--------------------------------------------------");
                         for (int i = 0; i < numTasks; i++) {
-                            System.out.println((i + 1) + ". " + taskList[i]);
+                            System.out.println((i + 1) + ". " + taskList.get(i));
                         }
                         System.out.println("--------------------------------------------------");
 
+                    break;
+
+                case "done": //remove task
+                    if (!inputParams.equals("")) {
+                        if ((Integer.parseInt(inputParams)) <= numTasks) {
+                            String taskToPrint = taskList.get((Integer.parseInt(inputParams)) - 1);
+                            taskList.remove(Integer.parseInt(inputParams) - 1);
+                            numTasks--;
+
+                            System.out.println("--------------------------------------------------");
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println("[✓] " + taskToPrint);
+                            System.out.println("--------------------------------------------------");
+                        } else { //factor in OOB
+                            System.out.println("--------------------------------------------------");
+                            System.out.println("Task does not exist.");
+                            System.out.println("--------------------------------------------------");
+
+
+                        }
+
+                    } else { //empty query, we simply treat this as adding task for now
+                        taskList.add(input);
+                        numTasks++;
+
+                        System.out.println("--------------------------------------------------");
+                        System.out.println("added: " + input);
+                        System.out.println("--------------------------------------------------");
+                    }
                     break;
 
                 case "bye": //quit the program
@@ -62,19 +99,18 @@ public class Duke {
 
                     break;
 
-                default:
-                    //add to task list and echo
-                    taskList[numTasks] = command;
-                    numTasks++;
+                default: //add to task list and echo
+                    if (!input.equals("")) {
+                        taskList.add(input);
+                        numTasks++;
 
-                    System.out.println("--------------------------------------------------");
-                    System.out.println("added: " + command);
-                    System.out.println("--------------------------------------------------");
+                        System.out.println("--------------------------------------------------");
+                        System.out.println("added: " + input);
+                        System.out.println("--------------------------------------------------");
+                    }
 
                     break;
             }
         }
     }
 }
-
-
